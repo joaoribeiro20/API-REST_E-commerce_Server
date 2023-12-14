96b3d9eb58caf954a59b4e4b18e5bb114b83b8da
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-12-14 10:20:01 -0300

    feat: create Entity User Role Pormission

---
+++ src/entities/user/User.ts
@@ -0,0 +1,40 @@
+import {
+    Column,
+    Entity,
+    getRepository,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    RelationId,
+  } from "typeorm";
+  import { BaseEntity } from "../BaseEntity";
+  import { Permission } from "./Permission";
+  import { Role } from "./Role";
+  
+  @Entity("users")
+  export class User extends BaseEntity {
+    @Column()
+    name: string;
+
+    @Column({unique:true})
+    email: string;
+  
+    @Column()
+    password: string;
+  
+    @ManyToMany(() => Role)
+    @JoinTable({
+      name: "users_roles",
+      joinColumns: [{ name: "user_id" }],
+      inverseJoinColumns: [{ name: "role_id" }],
+    })
+    roles: Role[];
+  
+    @ManyToMany(() => Permission)
+    @JoinTable({
+      name: "users_permissions",
+      joinColumns: [{ name: "user_id" }],
+      inverseJoinColumns: [{ name: "permission_id" }],
+    })
+    permissions: Permission[];
+  }
